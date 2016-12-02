d3a98fd5cd638b144d55aaa2737a6f2f23230ffd
pradeepwagle <pradeepwagle71@gmail.com>
2016-12-02 08:35:30 +0545

    added environment.yml

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class category (models.Model):
+    name=models.CharField(max_length=32)
+    description=models.CharField(max_length=120)
